c0f08e8f5a4660d670afacc5dac22e10f47d64ee
huangdijia <huangdijia@gmail.com>
2018-11-29 14:17:21 +0800

    support check_accinfo

--- src/AccessyouServiceProvider.php
+++ src/AccessyouServiceProvider.php
@@ -2,34 +2,41 @@
 
 namespace Huangdijia\Accessyou;
 
+use Huangdijia\Accessyou\Console\InfoCommand;
+use Huangdijia\Accessyou\Console\SendCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AccessyouServiceProvider extends ServiceProvider
 {
     protected $defer = true;
+
     protected $commands = [
-        'Huangdijia\\Accessyou\\Console\\AccessyouSendCommand',
-        'Huangdijia\\Accessyou\\Console\\AccessyouInfoCommand',
+        SendCommand::class,
+        InfoCommand::class,
     ];
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__.'/../config/config.php' => config_path('accessyou.php')]);
+            $this->publishes([__DIR__ . '/../config/config.php' => config_path('accessyou.php')]);
         }
     }
 
     public function register()
     {
-        $this->app->singleton('sms.accessyou', function () {
+        $this->app->singleton(Accessyou::class, function () {
             return new Accessyou(config('accessyou'));
         });
+
+        $this->app->alias(Accessyou::class, 'sms.accessyou');
+
         $this->commands($this->commands);
     }
 
     public function provides()
     {
         return [
+            Accessyou::class,
             'sms.accessyou',
         ];
     }
